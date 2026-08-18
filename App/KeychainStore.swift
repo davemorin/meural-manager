ca6e6a079412be0b8990f9b912f85062491bdb6f
Dave Morin <dave@morin.com>
2026-08-17 22:38:52 -0700

    Use App Store Connect bundle ID app.bitrig.davemorin.artwall

--- App/KeychainStore.swift
+++ App/KeychainStore.swift
@@ -3,7 +3,7 @@ import Security
 
 /// Minimal Keychain wrapper for storing the Meural account password.
 enum KeychainStore {
-  private static let service = "com.davemorin.Artwall"
+  private static let service = "app.bitrig.davemorin.artwall"
 
   static func save(_ value: String, forKey key: String) {
     delete(key)
